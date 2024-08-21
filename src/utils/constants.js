87444b26420414de3d5b1f0bd9afe40b6867ae7f
vigbi <bishoyibighneswar@gmail.com>
2024-08-22 02:44:51 +0530

    Added .env file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,4 +8,4 @@ export const API_OPTIONS = {
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
 
-export const OPENAI_KEY = "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";
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
